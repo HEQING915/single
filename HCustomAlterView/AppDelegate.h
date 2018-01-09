fb61877b21593617849a9f2714c2409185ab0114
何青 <qhe@iyouxin.com>
2018-01-09 08:57:37 +0800

    Initial Commit

---
+++ HCustomAlterView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HCustomAlterView
+//
+//  Created by 何青 on 2018/1/9.
+//  Copyright © 2018年 何青. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HCustomAlterView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HCustomAlterView
+//
+//  Created by 何青 on 2018/1/9.
+//  Copyright © 2018年 何青. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
